b6f590168b929129b0b2c37469e6838697603e0a
Shiv1209098 <40805710+Shiv1209098@users.noreply.github.com>
2018-08-18 00:35:44 -0400

    Update Temperature_closest.java

--- Temperature_closest.java
+++ Temperature_closest.java
@@ -1,6 +1,4 @@
-======================  CodinGame = 2 =========================================================================================================================
-
-Write a program that prints the temperature closest to 0 among input data. 
+/*Write a program that prints the temperature closest to 0 among input data. 
 If two numbers are equally close to zero, positive integer has to be considered 
 closest to zero (for instance, if the temperatures are -5 and 5, then display 5).
 
